f615bd135937ffa98e822733bebd4b1f304ea61b
Pip Cet <pipcet@gmail.com>
2018-08-15 16:26:35 +0000

    one more

--- src/lisp.h
+++ src/lisp.h
@@ -1218,6 +1218,7 @@ INLINE bool
 #define XSETCONDVAR(a, b) (XSETPSEUDOVECTOR (a, b, PVEC_CONDVAR))
 #define XSETBIGNUM(a, b) (XSETPSEUDOVECTOR (a, b, PVEC_BIGNUM))
 #define XSETMARKER(a, b) (XSETPSEUDOVECTOR (a, b, PVEC_MARKER))
+#define XSETOVERLAY(a, b) (XSETPSEUDOVECTOR (a, b, PVEC_OVERLAY))
 
 /* Efficiently convert a pointer to a Lisp object and back.  The
    pointer is represented as a Lisp integer, so the garbage collector
