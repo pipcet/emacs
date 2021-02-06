8a76b3c859755e719f82a5cd319c3d0f3dbf1e73
Pip Cet <pipcet@gmail.com>
2021-02-06 17:00:43 +0000

    src/fileio.c: I feel safer with this check in.

--- src/fileio.c
+++ src/fileio.c
@@ -711,7 +711,8 @@ This function does not grok magic file names.  */)
   if (!failed)
     {
       ptrdiff_t count = SPECPDL_INDEX ();
-      record_unwind_protect_int (close_file_unwind, fd);
+      if (fd > 0)
+	record_unwind_protect_int (close_file_unwind, fd);
       val = DECODE_FILE (val);
       if (STRINGP (text) && SBYTES (text) != 0)
 	write_region (text, Qnil, val, Qnil, Qnil, Qnil, Qnil, fd);
